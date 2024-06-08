baa938c51a6fde49457780205c626506ae233465
David <89795659+Tenvid@users.noreply.github.com>
2024-06-09 01:30:31 +0200

    Big refactor
    This commit makes a big refactor of the code,
    removes unnecessary files, adds requirements.txt
    and updates poetry.lock and poetry.toml

---
+++ frikibot/main.py
@@ -0,0 +1,56 @@
+import os
+from dotenv import load_dotenv
+from discord.ext import commands
+import logging
+import pokemon_generator
+
+load_dotenv()
+# Bot token obtained from the environment variable
+TOKEN = os.getenv("DISCORD_TOKEN")
+# Bot instance
+bot = pokemon_generator.bot
+
+logging.basicConfig(level="INFO", format="%(process)d-%(levelname)s-%(message)s")
+
+
+# Event realised when the bot is connected
+@bot.event
+async def on_ready():
+    print("Connected")
+
+
+# -hello
+@bot.command(name="hello", help="Says hello to user,")
+@commands.cooldown(1, 5, commands.BucketType.user)
+async def hello(ctx):
+    await ctx.send(f"Hola, {ctx.author.mention}!")
+
+
+@bot.command(name="test", help="test_command")
+async def test(ctx):
+    await ctx.send("""
+  ```ansi
+\u001b[0;31mAttack\u001b[0;0m
+```
+""")
+
+
+@commands.cooldown(1, 5, commands.BucketType.guild)
+@bot.command(name="pokemon", help="Generates a random Pokémon")
+async def pokemon(ctx):
+    embed, message = pokemon_generator.generate_random_pokemon(ctx)
+
+    await ctx.send(message, embed=embed)
+
+
+@bot.event
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.CommandOnCooldown):
+        await ctx.send(
+            f" {ctx.author.mention} This command is actually on cooldown, you can use it in"
+            f" {round(error.retry_after, 2)} seconds."
+        )
+
+
+# Bot start
+bot.run(TOKEN)
--- frikibot/pokemon_generator.py
+++ frikibot/pokemon_generator.py
@@ -1,11 +1,13 @@
-"""Generate a pokemon"""
 import random
+
+import discord
+from discord.ext import commands
+import requests
 import json
 import logging
-import discord
-import requests
+from typing import Dict
 
-from frikibot.database_handler import insert_registry
+bot = commands.Bot(command_prefix="-", intents=discord.flags.Intents().all())
 
 MIN_INDEX = 1
 MAX_INDEX = 1010
@@ -22,17 +24,16 @@ NAME_REPLACEMENTS = {
 
 
 def get_stats_string(name, decreased, increased):
-    """Transform stats into a string"""
     stats = get_pokemon_stats(name)
     logging.info("Stats generated")
     ret = "```ansi\n"
 
     if decreased is None and increased is None:
-        for stat in stats.items():
+        for stat in stats:
             actual_stat = stats[stat]
             ret += f"{stat}: {actual_stat}\n"
     else:
-        for stat in stats.items():
+        for stat in stats:
             actual_stat = stats[stat]
             if stat == decreased:
                 ret += f"\u001b[0;34m{stat}: {actual_stat} -\u001b[0;0m\n"
@@ -46,23 +47,23 @@ def get_stats_string(name, decreased, increased):
 
 
 def get_pokemon_stats(name):
-    """Get 'stats' element from a Pokémon into a dict"""
     ret = {
         "Attack": 0,
         "Defense": 0,
         "Special attack": 0,
         "Special defense": 0,
-        "Speed": 0
+        "Speed": 0,
     }
 
-    response_bis = requests.get(f"https://pokeapi.co/api/v2/pokemon/{name.replace('-gmax', '')}",
-                                timeout=10)
+    response_bis = requests.get(
+        f"https://pokeapi.co/api/v2/pokemon/{name.replace('-gmax', '')}"
+    )
 
-    stats = json.loads(response_bis.text)['stats']
+    stats = json.loads(response_bis.text)["stats"]
 
     stat_values = []
     for stat in stats:
-        stat_values.append(stat['base_stat'])
+        stat_values.append(stat["base_stat"])
 
     ret["Attack"] = stat_values[0]
     ret["Defense"] = stat_values[1]
@@ -76,32 +77,27 @@ def get_pokemon_stats(name):
 
 
 def get_random_nature(natures):
-    """Pick random nature"""
-    return random.choice(natures['results'])
+    return random.choice(natures["results"])
 
 
 def get_moves_string(name: str):
-    """Build a string using random moves of the ones
-    that a Pokémon can learn"""
     moves = get_pokemon_moves(name)
     ret = "```\n"
     for move in moves:
-        ret = ret.join(f"\n {move} ")
-    ret = ret.join("```")
+        ret += f"\n {move} "
+    ret += "```"
 
     logging.info(f"Moves: {moves}")
     return ret
 
 
 def eliminate_invalid_forms(varieties):
-    """Filter Pokémon forms that cannot be printed correctly """
     variety = random.choice(varieties)
-    ret_name = str(variety['pokemon']['name'])
+    ret_name = str(variety["pokemon"]["name"])
     return correct_name(ret_name, varieties, variety)
 
 
-def correct_name(ret_name: str, varieties: {}, variety: {}):
-    """Fix name differencies between PokeAPI and image source"""
+def correct_name(ret_name: str, varieties: Dict, variety: Dict):
     if "pikachu" in ret_name:
         if 1 <= varieties.index(variety) <= 6 or varieties.index(variety) == 14:
             return eliminate_invalid_forms(varieties)
@@ -118,15 +114,13 @@ def correct_name(ret_name: str, varieties: {}, variety: {}):
 
 
 def correct_suffixes(name):
-    """Fix suffixes in Pokémon names"""
-    for rep in NAME_REPLACEMENTS.items():
+    for rep in NAME_REPLACEMENTS:
         name = name.replace(rep, NAME_REPLACEMENTS[rep])
 
     return name
 
 
 def get_shiny_chance():
-    """Generate a random number to determine if Pokémon should be shiny"""
     index = random.randint(1, 100)
     if index <= 10:
         return "shiny"
@@ -134,26 +128,27 @@ def get_shiny_chance():
 
 
 def generate_image_url(name):
-    """Build image url"""
     colour = get_shiny_chance()
     url = f"https://img.pokemondb.net/sprites/home/{colour}/{name}.png"
     return url, colour
 
 
 def get_random_move(moves):
-    """Return a random move picked from the possible moves of a Pokémon"""
-    return moves[random.randint(0, len(moves) - 1)]['move']['name'].replace("-", " ").capitalize()
+    return (
+        moves[random.randint(0, len(moves) - 1)]["move"]["name"]
+        .replace("-", " ")
+        .capitalize()
+    )
 
 
 def get_pokemon_moves(name):
-    """Get four random moves from the ones that a Pokémon can learn"""
     ret = []
     try:
         name = name.replace("-gmax", "")
         url = f"https://pokeapi.co/api/v2/pokemon/{name}"
-        response = requests.get(url, timeout=10)
+        response = requests.get(url)
         jres = json.loads(response.text)
-        moves = jres['moves']
+        moves = jres["moves"]
         while True:
             move = get_random_move(moves)
             if move not in ret:
@@ -165,47 +160,35 @@ def get_pokemon_moves(name):
     return ret
 
 
+def get_normal_or_shiny():
+    return "shiny" if random.randint(1, 100) <= 10 else "normal"
+
+
+def get_pokemon_variety(loaded_response: Dict):
+    return random.choice(loaded_response["varieties"])
+
+
 def generate_random_pokemon(ctx):
-    """Create a random Pokémon and send information to DB Handler"""
+    color = get_normal_or_shiny()
 
-    author = ctx.author
+    embed = build_embed(color)
 
-    pokemon_index = get_random_index()
-
-    url = get_url(pokemon_index)
-
-    response = load_pokemon(requests.get(url, timeout=10))
-
-    colour, moves, name, nature, sprite_url = get_pokemon_elements(response)
-
-    decreased, increased = get_changed_stats(nature)
-
-    stats_string = get_stats_string(name, decreased, increased)
-
-    embed = build_embed(moves, stats_string, name,
-                        nature, sprite_url, pokemon_index)
-
-    message = get_message(colour, ctx)
-
-    insert_registry(author, response, moves, nature["name"])
+    message = get_message(color, ctx)
 
     return embed, message
 
 
 def get_message(colour, ctx):
-    """Create the message depending of the colour of the Pokémon"""
     message = f"{ctx.author.mention} Here you have your Pokémon"
     if colour == "shiny":
         message = message.replace("Pokémon", "✨SHINY✨ Pokémon")
-    logging.info("Message obtained")
     return message
 
 
 def get_changed_stats(nature):
-    """Get the stats modified by the nature"""
     try:
-        decreased = nature['decreased_stat']['name'].replace("-", " ")
-        increased = nature['increased_stat']['name'].replace("-", " ")
+        decreased = nature["decreased_stat"]["name"].replace("-", " ")
+        increased = nature["increased_stat"]["name"].replace("-", " ")
     except TypeError:
         decreased = None
         increased = None
@@ -221,30 +204,22 @@ def get_changed_stats(nature):
     return decreased, increased
 
 
-def insert_pokemon_in_database():
-    """TODO: Not implemented yet"""
-
-
 def get_pokemon_elements(response):
-    """Get all the elements from a Pokémon"""
-    poke = response
+    poke = load_pokemon(response)
 
-    variety = random.choice(poke['varieties'])
+    variety = random.choice(poke["varieties"])
 
-    name = variety['pokemon']['name']
+    name = variety["pokemon"]["name"]
 
-    colour, sprite_url = get_sprite_color(name, poke['varieties'], variety)
+    colour, sprite_url = get_sprite_color(name, poke["varieties"], variety)
 
     moves = get_moves_string(name)
 
     nature = get_nature()
-
-    insert_pokemon_in_database()
     return colour, moves, name, nature, sprite_url
 
 
 def get_sprite_color(name, varieties, variety):
-    "Build url and get colour"
     name = correct_name(name, varieties, variety)
     sprite_url, colour = generate_image_url(name)
     logging.info(msg=f"Sprite URL: {sprite_url}")
@@ -253,52 +228,36 @@ def get_sprite_color(name, varieties, variety):
 
 
 def validate_name(poke):
-    """Validate the name of the Pokémon fixing naming problems"""
-    name = eliminate_invalid_forms(poke['varieties'])
+    name = eliminate_invalid_forms(poke["varieties"])
     logging.info(msg=f"NAME:{name}")
     return name
 
 
 def load_pokemon(response):
-    """Transform string response into an object"""
     poke = json.loads(response.text)
     logging.info(msg=f"\nPokemon response: {poke['name']}")
     return poke
 
 
 def get_nature():
-    "Get a random nature from all the available"
-    natures = json.loads(requests.get("https://pokeapi.co/api/v2/nature", timeout=10).text)
+    natures = json.loads(requests.get("https://pokeapi.co/api/v2/nature").text)
     nature = pick_nature(natures)
     logging.info(f"Nature: {nature['name']}")
     return nature
 
 
-def get_url(pokemon_index):
-    """Generate url using Pokémon index"""
-    url = f"https://pokeapi.co/api/v2/pokemon-species/{pokemon_index}"
-    return url
-
-
-def get_random_index():
-    """Generate a random pokedex number"""
-    pokemon_index = random.randint(MIN_INDEX, MAX_INDEX)
-    return pokemon_index
-
-
 def pick_nature(natures):
-    """Select a random nature object"""
     random_nature = get_random_nature(natures)
-    detailed_nature = requests.get(random_nature['url'], timeout=10)
+    detailed_nature = requests.get(random_nature["url"])
     loaded_detailed_nature = json.loads(detailed_nature.text)
     return loaded_detailed_nature
 
 
-def build_embed(moves_string: str, stats_string: str, pokemon_name: str,
-                nature, sprite_url, index: int):
+def build_embed(color: str):
     """
     Creates an Embed from Discord
 
+    :param index: Pokédex number
     :param moves_string: Formatted string which contains the moves of the Pokémon
     :param stats_string: Formatted string which contains the stats of the Pokémon
     :param pokemon_name: Random generated name
@@ -306,18 +265,38 @@ def build_embed(moves_string: str, stats_string: str, pokemon_name: str,
     :param sprite_url: URL of the sprite
     :return: Formatted embed
     """
+
+    pokemon_index = random.randint(MIN_INDEX, MAX_INDEX)
+
+    url = f"https://pokeapi.co/api/v2/pokemon-species/{pokemon_index}"
+
+    response = requests.get(url)
+
+    pokemon_name = get_pokemon_variety(json.loads(response.text))["pokemon"]["name"]
+
+    nature = get_nature()
+
+    decreased, increased = get_changed_stats(nature)
+
     ret = discord.Embed()
 
-    ret.set_image(url=sprite_url)
+    ret.set_image(
+        url=f"https://img.pokemondb.net/sprites/home/{color}/{pokemon_name}.png"
+    )
+
     # Pokemon Moves
-    ret.add_field(name="Moves", value=moves_string)
+    ret.add_field(name="Moves", value=get_moves_string(pokemon_name))
 
     # Pokemon Stats
-    ret.add_field(name="Stats", value=stats_string)
+    ret.add_field(
+        name="Stats", value=get_stats_string(pokemon_name, decreased, increased)
+    )
 
     # Pokémon name
-    ret.title = f"# {index} *{nature['name'].capitalize()}* {pokemon_name.capitalize()}"
-    logger.info(msg="Embed title setted")
+    ret.title = (
+        f"# {pokemon_index} *{nature['name'].capitalize()}* {pokemon_name.capitalize()}"
+    )
+    logger.info(msg="Embed title set")
 
     logging.info("Embed built")
 

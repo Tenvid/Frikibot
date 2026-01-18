53e92f06fdf3d6cf586b37403e3b0b122421a7a4
David Gómez Barberá <dagobar.pk.db@gmail.com>
2026-01-18 20:13:25 +0100

    feat: Add method get_all_by_trainer to pokemon repository

--- frikibot/infrastructure/sqlalchemy_pokemon_repository.py
+++ frikibot/infrastructure/sqlalchemy_pokemon_repository.py
@@ -1,16 +1,16 @@
 """SQLAlchemy implementation of the Pokemon repository."""
 
+import sqlalchemy
 from sqlalchemy.orm import Session
 
 from frikibot.db.models import pokemon
 from frikibot.domain import pokemon_repository
-from frikibot.infrastructure.database import SESSION
 
 
 class SQLAlchemyPokemonRepository(pokemon_repository.PokemonRepository):
     """Concrete implementation of PokemonRepository using SQLAlchemy."""
 
-    def __init__(self, session: Session = SESSION) -> None:
+    def __init__(self, session: Session) -> None:
         """Initialize the repository with a SQLAlchemy session."""
         self.session = session
 
@@ -22,5 +22,21 @@ class SQLAlchemyPokemonRepository(pokemon_repository.PokemonRepository):
             pokemon (pokemon_repository.Pokemon): The Pokemon to add.
 
         """
-        self.session.add(pokemon)
-        self.session.commit()
+        try:
+            self.session.add(pokemon)
+            self.session.commit()
+        except sqlalchemy.exc.SQLAlchemyError:
+            self.session.rollback()
+
+    def get_all_by_trainer(self, trainer_id: int) -> list[pokemon.Pokemon]:
+        """
+        Get all Pokemons for a specific trainer.
+
+        Args:
+            trainer_id (int): The ID of the trainer.
+
+        Returns:
+            list[pokemon.Pokemon]: A list of Pokemons owned by the trainer.
+
+        """
+        return self.session.query(pokemon.Pokemon).filter(pokemon.Pokemon.trainer_id == trainer_id).all()
